d31e67f88448d64835a5d86de7fd8ae45375dd55
Daniel Kofler <fox27374@gmail.com>
2020-07-08 10:58:34 +0200

    Added MQTT functionality

--- sensorpi-web.py
+++ sensorpi-web.py
@@ -1,9 +1,39 @@
+#!/usr/bin/env python
+
+import eventlet
 from flask import Flask, render_template
-app = Flask(__name__)
+from flask_mqtt import Mqtt
+from flask_socketio import SocketIO
+
+eventlet.monkey_patch()
+
+app = Flask(__name__, template_folder='./views')
+app.config['MQTT_BROKER_URL'] = '10.140.61.20'
+app.config['MQTT_BROKER_PORT'] = 1883
+app.config['MQTT_REFRESH_TIME'] = 1.0
+
+mqtt = Mqtt(app)
+socketio = SocketIO(app)
+
+@mqtt.on_connect()
+def handle_connect(client, userdata, flags, rc):
+    mqtt.subscribe('sensorpi/log')
+
+@mqtt.on_message()
+def handle_mqtt_message(client, userdata, message):
+    data = dict(
+        topic=message.topic,
+        payload=message.payload.decode()
+    )
+    # emit a mqtt_message event to the socket containing the message data
+    socketio.emit('mqtt_message', data=data)
 
 @app.route('/')
 def index():
-   return render_template("index.html")
+    return render_template('graph.html')
 
-if __name__ == '__main__':
-   app.run()
+@mqtt.on_log()
+def handle_logging(client, userdata, level, buf):
+    print(level, buf)
+
+socketio.run(app, host='localhost', port=5000, use_reloader=True, debug=True)
