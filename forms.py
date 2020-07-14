c462b90d403c4973a95d5ce140ce39db199fd794
fox27374 <fox27374@gmail.com>
2020-07-14 17:39:42 +0200

    initial

--- forms.py
+++ forms.py
@@ -1,12 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField
+from wtforms.validators import InputRequired, IPAddress, Length
 
 class configForm(FlaskForm):
     sensorPiInterface = SelectField('Interface', choices=[('wlan0', 'wlan0'), ('wlan1', 'wlan1')])
     sensorPiChannels = StringField('Channels')
-    mqttServer = StringField('MQTT Server')
-    mqttPort = StringField('MQTT Server Port')
-    splunkServer = StringField('Splunk Server')
+    mqttServer = StringField('MQTT Server', [InputRequired(), IPAddress(ipv4=True)])
+    mqttPort = StringField('MQTT Server Port', [InputRequired()])
+    splunkServer = StringField('Splunk Server', [IPAddress(ipv4=True)])
     splunkPort = StringField('Splunk Server Port')
     splunkToken = StringField('Splunk Token')
     submit = SubmitField('Submit')
